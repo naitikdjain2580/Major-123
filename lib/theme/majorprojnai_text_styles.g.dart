7d501d3b3123673c670122b54c71e883d55595f9
Parabeac-Nest <admin@parabeac.com>
2023-03-08 11:09:07 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/majorprojnai_text_styles.g.dart
@@ -0,0 +1,50 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class MajorprojnaiTextStyles {
+
+static const TextStyle h5 = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Noto Serif',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle h4 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0,
+  fontFamily: 'Noto Serif',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle description = TextStyle(
+  fontSize: 13,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Roboto',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+
+static const TextStyle uI16Medium = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  letterSpacing: 0,
+  fontFamily: 'Inter',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
